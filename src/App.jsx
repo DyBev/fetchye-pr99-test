67f2d8bbc561b1cf0444eb4d7c2f6dd19058ed97
DyBev <54762882+DyBev@users.noreply.github.com>
2025-07-04 11:33:15 +0100

    chore(init): batman commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useFetchye } from 'fetchye'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [id, setId] = useState(1);
+  useEffect(() => {
+    setTimeout(() => setId(2), 1000);
+  });
+
+  const { data, error, isLoading } = useFetchye(id);
+  console.log({ data, error, isLoading });
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
